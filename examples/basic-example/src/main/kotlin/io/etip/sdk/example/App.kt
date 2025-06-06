25c71c91d29d0c573fc061bab58c6b4c3ec3656d
Lucas Lopes <lucas.lopesifrn@gmail.com>
2025-06-06 12:25:40 -0300

    Add DI (#31)

--- examples/basic-example/src/main/kotlin/io/etip/sdk/example/App.kt
+++ examples/basic-example/src/main/kotlin/io/etip/sdk/example/App.kt
@@ -1,39 +1,22 @@
 package io.etip.sdk.example
 
 import io.etip.sdk.api.pokemon.config.Environment
-import io.etip.sdk.api.pokemon.config.Pokemon
+import io.etip.sdk.api.pokemon.config.PokemonConfiguration
 import io.etip.sdk.api.pokemon.di.ApiRegistry
 import io.etip.sdk.api.pokemon.di.AppModule
 import io.etip.sdk.api.pokemon.integration.PokemonApiService
-import io.github.cdimascio.dotenv.Dotenv
 import kotlinx.coroutines.runBlocking
-import org.slf4j.LoggerFactory
 
 fun main() = runBlocking {
-    val logger = LoggerFactory.getLogger("Pokemon")
-    val dotenv = Dotenv.configure().ignoreIfMissing().load()
 
-    val pokemon = Pokemon.fromEnv()
-
-    val context = pokemon.getContext()
-    val secrets = pokemon.getSecrets()
-    val configs = pokemon.getConfigs()
-
-    val runMode = context["runMode"]
-
-    logger.info("Run mode: $runMode")
-
-    logger.info("------------------------CONFIG------------------------")
-    logger.info(configs.toString())
-    logger.info("------------------------SECRET------------------------")
-    logger.info(secrets.toString())
-    logger.info("------------------------CONTEXT------------------------")
-    logger.info(context.toString())
-    logger.info("------------------------ENV----------------------------")
-    logger.info(dotenv.entries().toString())
-    AppModule.init(Environment.STAGING)
+    val config = PokemonConfiguration(
+        baseUrl = "https://pokeapi.co/",
+        environment = Environment.STAGE,
+        enableLogging = true
+    )
+    AppModule.init(config)
 
     val pokemonDetail = ApiRegistry.getApi<PokemonApiService>().getPokemonByName("pikachu")
-    //val pokemonName = ApiRegistry.getApi<PokemonApiService>().getPokemonByName("pikachu")
     println(pokemonDetail)
+    
 }
--- sdk-api/src/main/kotlin/io/etip/sdk/api/pokemon/di/AppModule.kt
+++ sdk-api/src/main/kotlin/io/etip/sdk/api/pokemon/di/AppModule.kt
@@ -1,29 +1,19 @@
 package io.etip.sdk.api.pokemon.di
 
-import io.etip.sdk.api.pokemon.config.Environment
 import io.etip.sdk.api.pokemon.config.PokemonConfiguration
-import io.etip.sdk.api.pokemon.config.PokemonConfigurationFactory
-import io.etip.sdk.api.pokemon.config.mock.MockConfigProvider
 import io.etip.sdk.api.pokemon.integration.PokemonApiService
 import io.etip.sdk.api.pokemon.integration.impl.DefaultPokemonApiService
 import io.etip.sdk.core.BaseHttpClient
 import io.etip.sdk.core.HttpClientFactory
 
 object AppModule {
-    fun init(environment: Environment) {
-        val config = PokemonConfigurationFactory(MockConfigProvider).create()
-        val pokemonConfig =
-            PokemonConfiguration(
-                baseUrl = config.baseUrl,
-                environment = environment,
-                enableLogging = true,
-                timeoutMillis = 30_000,
-            )
 
-        val client = HttpClientFactory.create(pokemonConfig)
+    fun init(config: PokemonConfiguration) {
+        val client = HttpClientFactory.create(config)
         val baseHttpClient = BaseHttpClient(client)
         val service: PokemonApiService = DefaultPokemonApiService(baseHttpClient)
 
         ApiRegistry.registerApi(service)
     }
+    
 }
