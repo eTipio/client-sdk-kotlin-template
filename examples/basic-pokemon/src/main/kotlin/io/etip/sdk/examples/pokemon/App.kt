6d1d3025bb3085a0fbe50fc3055464df57a6db3b
Chrys <chrysexaucet@hotmail.fr>
2025-08-15 05:31:52 +0000

    simplify demo

--- examples/basic-pokemon/src/main/kotlin/io/etip/sdk/examples/pokemon/App.kt
+++ examples/basic-pokemon/src/main/kotlin/io/etip/sdk/examples/pokemon/App.kt
@@ -1,21 +1,32 @@
 package io.etip.sdk.examples.pokemon
 
 import io.etip.sdk.api.pokemon.config.PokemonConfiguration
-import io.etip.sdk.examples.pokemon.di.AppModule
 import io.etip.sdk.api.pokemon.integration.PokemonService
+import io.etip.sdk.api.pokemon.integration.impl.PokeApiService
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.dsl.module
 
 object App : KoinComponent {
     suspend fun run() {
         val config = PokemonConfiguration(baseUrl = "https://pokeapi.co/")
-        // assume service is used in many places, so we initialize it once
-        // and inject it where needed
-        AppModule.init(config)
+        val pokeApiService: PokemonService = PokeApiService.create(config)
 
+        // we just assume the service is used in many places, so we initialize it once
+        // and inject it where needed. If not useful, you can just use the service directly
+        // but Koin allows for better dependency management and testing.
+        startKoin {
+            modules(
+                module {
+                    single<PokemonService> { pokeApiService }
+                },
+            )
+        }
+
+        // Now we can use the service in our application
         val service: PokemonService by inject()
         val pokemon = service.getByName("pikachu")
-
         println("Found pokemon Name : ${pokemon.name}, ID : ${pokemon.id}, Height : ${pokemon.height}, Weight : ${pokemon.weight}")
     }
 }
